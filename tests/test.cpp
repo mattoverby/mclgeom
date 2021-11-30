c6e397b06c3378da246db43fc013f42d551ff153
mattoverby <mattoverby@gmail.com>
2021-11-30 11:41:55 -0600

    added lbfgs

--- tests/test.cpp
+++ tests/test.cpp
@@ -5,17 +5,20 @@
 #include "MCL/AssertHandler.hpp"
 #include "MCL/Projection.hpp"
 #include "MCL/Barycoords.hpp"
+#include "MCL/LBFGS.hpp"
 
 using namespace Eigen;
 
 void test_projection_barys();
+void test_lbfgs();
 
 // Should probably replace with actual unit testing through CTest
 int main(int argc, char *argv[])
 {
     (void)(argc);
     (void)(argv);
-    test_projection_barys();
+    //test_projection_barys();
+    test_lbfgs();
     return EXIT_SUCCESS;
 }
 
@@ -32,4 +35,27 @@ void test_projection_barys()
     mclAssert(barys.maxCoeff() > 1);
     mclAssert(proj_barys.maxCoeff() < 1);
     std::cout << barys.transpose() << " " << proj_barys.transpose() << std::endl;
+}
+
+void test_lbfgs()
+{
+
+    mcl::LBFGS<Vector2d> solver;
+    solver.gradient = [&](const Eigen::Vector2d& x, Eigen::Vector2d& g)->double
+    {
+        if (g.rows() == x.rows())
+        {
+            g[0] = 400*x[0]*x[0]*x[0] - 400*x[0]*x[1] + 2*x[0] - 2;
+            g[1] = 200*(x[1] - x[0]*x[0]);
+        }
+        double a = 1.0 - x[0];
+        double b = x[1] - x[0]*x[0];
+        return a*a + b*b*100.0;
+    };
+    
+    Vector2d x = Vector2d::Random();
+    std::cout << "x init: " << x.transpose() << std::endl;
+    double obj = solver.minimize(x);
+    std::cout << "obj: " << obj << ", x: " << x.transpose() << std::endl;
+
 }
